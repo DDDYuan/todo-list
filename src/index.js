1764f76df05df0afc518dd1bcdc8906d4f9a3c7f
Di Yuan <dyuan@thoughtworks.com>
2018-07-24 14:07:14 +0800

    add dependency of bootstrap

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import registerServiceWorker from "./registerServiceWorker";
-
+import "bootstrap/dist/css/bootstrap.min.css";
 import Todo from "./Todo";
 
 ReactDOM.render(<Todo />, document.getElementById("root"));
